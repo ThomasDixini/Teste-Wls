ff2333924e8c7486677f143b54a3575089e0851b
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2022-03-11 16:41:08 -0300

    Componente Header Criado

--- styles/global.ts
+++ styles/global.ts
@@ -20,6 +20,17 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
 }
 
+
+html {
+    @media(max-width: 1080px) {
+        font-size: 93.75%;
+    }
+
+    @media(max-width: 720px) {
+        font-size: 87.5%;
+    }
+}
+
 body {
     font-family: "Roboto", sans-serif;
     font: 400 1rem;
