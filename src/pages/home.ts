1f07e4ecd4807c3c67892481bd9996dd5f9ab617
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2022-03-11 17:39:04 -0300

    Estruturando o html da lista de tarefas com o botão de procurar, e estilizando o botão

---
+++ src/pages/home.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+
+export const Container = styled.main`
+
+max-width: 375px;
+width: 100%;
+    
+padding: 1rem;
+
+    .search {
+
+        display: flex;
+        justify-content: center;
+
+        input {
+            padding: 0.5rem;
+            height: 3rem;
+            margin: 2rem auto; 
+
+            border-radius: 0.5rem;
+            border: none;
+
+            width: 100%;
+
+            background: var(--black-lowOpacity);
+        }
+
+}
+
+
+`
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,55 @@
+import { Container } from "./home";
+
+import searchImg from '../../public/images/search.svg';
 
 
 export default function Home() {
   return (
-    <>
-      Hello World
-    </>
+    <Container>
+
+      <section className="search">
+        
+          <input type="text" placeholder="Procurar tarefas" />
+        
+      </section>
+
+      <section className="tasks-list">
+        <h1>
+          Tarefas
+        </h1>
+
+        <div className="task">
+          
+          <span>
+            Nome:
+            <img src="/images/etc.svg" alt="Etc" />
+          </span>
+
+          <p>Descrição da tarefa</p>
+
+          <div>
+            <img src="/images/checked.svg" alt="Concluído" />
+            Concluído
+          </div>
+
+        </div>
+
+        <div className="task">
+
+          <span>
+            Nome:
+            <img src="/images/etc.svg" alt="Etc" />
+          </span>
+
+          <p>Descrição da tarefa</p>
+
+          <div>
+            <img src="/images/checked.svg" alt="Concluído" />
+            Concluído
+          </div>
+
+        </div>
+      </section>
+    </Container>
   )
 }
